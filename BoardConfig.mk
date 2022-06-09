e146b8844e0ac709fbf683ff4c70a89296835e47
Nicholas Lim <nicknitewolf@lineageos.org>
2022-06-09 22:16:09 +0800

    q6x: Add ssv6051 support
    Change-Id: If5b34c8dd564b943f9b26858dccea6f37f0552d0

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,3 +16,9 @@ TARGET_DTB_NAME := gxl_p281_1g gxl_p281_2g
 
 ## Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
+
+## Wi-Fi
+BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_icomm
+BOARD_WLAN_DEVICE := icomm
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_icomm
+WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
--- device.mk
+++ device.mk
@@ -13,3 +13,15 @@ $(call inherit-product, vendor/amlogic/q6x/q6x-vendor.mk)
 ## Factory
 PRODUCT_HOST_PACKAGES += \
     aml_image_packer
+
+# Wi-Fi
+PRODUCT_CFI_INCLUDE_PATHS += \
+    hardware/amlogic/kernel-modules/icomm/wpa_supplicant_8_lib
+
+PRODUCT_PACKAGES += \
+    ssv6051
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/init-files/init.amlogic.wifi_buildin.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.amlogic.wifi_buildin.rc \
+    hardware/amlogic/kernel-modules/icomm/ssv6051/ssv6051-wifi.cfg:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/ssv6051/ssv6051-wifi.cfg \
+    hardware/amlogic/kernel-modules/icomm/ssv6051/image/ssv6051-sw.bin:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/ssv6051/ssv6051-sw.bin
